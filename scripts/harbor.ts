910687322f459ad06b65e2a0d7e2a6874a9b84d5
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-26 14:55:45 -0500

    add stats to shipyard

--- scripts/harbor.ts
+++ scripts/harbor.ts
@@ -6,7 +6,16 @@ import {
   getShopItems,
   type ShipData,
 } from "./storage";
-import { getDoubloonsPerHour, isShipShipped, truncateTo } from "./util";
+import {
+  getAvgDoubloonsPerProject,
+  getAvgHoursPerProject,
+  getDoubloonsPerHour,
+  getTotalDoubloons,
+  getTotalHours,
+  htmlToNode,
+  isShipShipped,
+  truncateTo,
+} from "./util";
 
 function setupObservers(onPageChange: () => void) {
   window.addEventListener("load", function () {
@@ -107,22 +116,58 @@ function injectDoubloonsPerHour(
 const SHIP_ESTIMATED_DOUBLOONS_PREFIX =
   "DJLASER-estimatedDoubloons-shipped-ship-";
 
+function pill(text: string, image: string) {
+  return `<span id="" class="inline-flex items-center gap-1 rounded-full px-2 border text-sm leading-none text-gray-600 bg-gray-50 border-gray-500/10 false " data-sentry-component="Pill" data-sentry-source-file="pill.tsx" style="vertical-align: middle;">${image}<span class="inline-block py-1">${text}</span></span>`;
+}
+
+function doubloonsPill(text: string) {
+  return pill(
+    text,
+    `<img alt="doubloons" loading="lazy" width="16" height="20" decoding="async" data-nimg="1" src="/_next/static/media/doubloon.fd63888b.svg" style="color: transparent;">`,
+  );
+}
+
+function timePill(text: string) {
+  return pill(
+    text,
+    `<svg fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414" xmlns="http://www.w3.org/2000/svg" aria-label="clock" viewBox="0 0 32 32" preserveAspectRatio="xMidYMid meet" fill="currentColor" width="20" height="20" style="display: inline-block; vertical-align: middle;"><g><path fill-rule="evenodd" clip-rule="evenodd" d="M26 16c0 5.523-4.477 10-10 10S6 21.523 6 16 10.477 6 16 6s10 4.477 10 10zm2 0c0 6.627-5.373 12-12 12S4 22.627 4 16 9.373 4 16 4s12 5.373 12 12z"></path><path d="M15.64 17a1 1 0 0 1-1-1V9a1 1 0 0 1 2 0v7a1 1 0 0 1-1 1z"></path><path d="M21.702 19.502a1 1 0 0 1-1.366.366l-5.196-3a1 1 0 0 1 1-1.732l5.196 3a1 1 0 0 1 .366 1.366z"></path></g></svg>`,
+  );
+}
+
 const SHIPYARD_STATS_ID = "DJLASER-shipyard-stats";
 const SHIPYARD_STATS_CLASSES =
-  "rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn flex flex-col sm:gap-2 sm:flex-row items-start sm:items-center p-4 hover:bg-gray-100 transition-colors duration-200";
+  "rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn flex flex-col gap-2 items-start items-start p-4";
 
 function injectStats(ships: ShipData[]) {
   if (document.getElementById(SHIPYARD_STATS_ID)) return;
-  const doubloonsPerHour = getDoubloonsPerHour(ships);
 
   const shipContainerElement = (
     (document.getElementById("radix-:r0:-content-shipyard") ||
       document.getElementById("radix-:rd:-content-shipyard")) as HTMLDivElement
   ).children[0].children[1].children[3].children[0];
 
-  const statsElement = document.createElement("div");
-  statsElement.className = SHIPYARD_STATS_CLASSES;
-  statsElement.id = SHIPYARD_STATS_ID;
+  const doubloonsPerProject = getAvgDoubloonsPerProject(ships);
+  const hoursPerProject = getAvgHoursPerProject(ships);
+  const doubloonsPerHour = getDoubloonsPerHour(ships);
+  const doubloonsEarned = getTotalDoubloons(ships);
+  const timeSpent = getTotalHours(ships);
+
+  const statsElement = htmlToNode(
+    `<div id="${SHIPYARD_STATS_ID}" class="${SHIPYARD_STATS_CLASSES}">
+      <div class="flex flex-wrap items-center gap-3 text-sm">
+      <h2 class="text-xl font-semibold text-center">All time:</h2>
+        ${doubloonsPill(`${doubloonsEarned} doubloons earned`)}
+        ${timePill(`${truncateTo(timeSpent, 10)} hours shipped`)}
+      </div>
+      <div class="flex flex-wrap items-center gap-3 text-sm">
+      <h2 class="text-xl font-semibold text-center">Average:</h2>
+        ${doubloonsPill(`${truncateTo(doubloonsPerHour, 10)} per hour`)}
+        ${doubloonsPill(`${truncateTo(doubloonsPerProject, 10)} per project`)}
+        ${timePill(`${truncateTo(hoursPerProject, 10)} hours per project`)}
+      </div>
+    </div>`,
+  );
+
   console.log(statsElement);
 
   shipContainerElement.insertBefore(
--- scripts/util.ts
+++ scripts/util.ts
@@ -20,6 +20,19 @@ export function getDoubloonsPerHour(ships: ShipData[]): number {
   return getTotalDoubloons(ships) / getTotalHours(ships);
 }
 
+export function getAvgDoubloonsPerProject(ships: ShipData[]): number {
+  return (
+    getTotalDoubloons(ships) /
+    ships.filter((ship) => ship.totalDoubloons > 0).length
+  );
+}
+
+export function getAvgHoursPerProject(ships: ShipData[]): number {
+  return (
+    getTotalHours(ships) / ships.filter((ship) => ship.paidHours > 0).length
+  );
+}
+
 // True if a ship was ever shipped, even if there are draft updates
 export function isShipShipped(ship: ShipData) {
   for (const update of ship.updates) {
@@ -27,4 +40,19 @@ export function isShipShipped(ship: ShipData) {
   }
 
   return false;
+}
+
+export function htmlToNode<T extends ChildNode>(html: string): T {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  const nNodes = template.content.childNodes.length;
+  if (nNodes !== 1) {
+    throw new Error(
+      `html parameter must represent a single node; got ${nNodes}. ` +
+        "Note that leading or trailing spaces around an element in your " +
+        'HTML, like " <img/> ", get parsed as text nodes neighbouring ' +
+        "the element; call .trim() on your input to avoid this.",
+    );
+  }
+  return template.content.firstChild as T;
 }
