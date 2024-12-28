bfc7cb7ed3b4e7bb3c815da982a434032611a785
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-28 13:26:18 -0500

    Much improved looks, plus cool loading animation

--- pages/popup/components/generic/Card.tsx
+++ pages/popup/components/generic/Card.tsx
@@ -4,14 +4,9 @@ export const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(function Card({ className, ...props }, ref) {
-  const backgroundColor = "#fffffff0";
-
   return (
     <div
       ref={ref}
-      style={{
-        backgroundColor,
-      }}
       className={
         "rounded-lg bg-card text-card-foreground shadow-sm bg-blend-color-burn " +
         className
